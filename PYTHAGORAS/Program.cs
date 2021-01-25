ad9a01b6acf2e721bc20ca80c2bb1d87f2784647
Rasmus <Rasmus@vinperlen.dk>
2021-01-25 12:44:23 +0100

    Add project files.

---
+++ PYTHAGORAS/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PYTHAGORAS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pythagoras");
+            Console.WriteLine("Bestem a^2");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Bestem b^2");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"c^2 = {(a * a) + (b * b)}");
+            Console.ReadKey();
+        }
+    }
+}
